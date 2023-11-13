933db0cf6186b89c0e224dbcf49a963d3af6fed0
Mahidhar748 <mahidhar874@gmail.com>
2023-11-13 10:39:03 +0530

    java file is added

---
+++ Main.java
@@ -0,0 +1,12 @@
+package Java_collection;
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("hello World");
+        int a=sc.nextInt(),b = sc.nextInt();
+        System.out.println(a+b);
+        sc.close();
+    }
+
+}
